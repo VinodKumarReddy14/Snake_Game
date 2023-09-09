176ba66b8cb05861203bc79c2a1ef22b80da6372
VinodKumarReddy14 <imvinodreddy14@gmail.com>
2023-09-09 16:42:10 +0530

    To track of High Score

--- scoring.py
+++ scoring.py
@@ -5,7 +5,8 @@ class Scoring(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -19,12 +20,10 @@ class Scoring(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode='w') as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.clear()
-    #     self.write(f"GAME OVER! Your Score: {self.score}", align="center", font=('Arial', 18, 'normal'))
 
     def gain_score(self):
         self.score += 1
